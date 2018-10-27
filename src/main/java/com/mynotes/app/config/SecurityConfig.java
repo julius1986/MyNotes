7f40f5db286617ceb92d06bbc166b41ef1f4fc57
Yuliy <uliy86@gmail.com>
2018-10-27 16:33:29 +0200

    add spring security to pom.xml and add base configuration for security

---
+++ src/main/java/com/mynotes/app/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.mynotes.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityEnablerConfiguration {
+
+	protected void configure(HttpSecurity http) throws Exception {
+ 		http.authorizeRequests()
+ 				.antMatchers("/users/public/**").permitAll()
+ 				.antMatchers("/users/private/**").hasAnyRole("USER")
+ 				
+ 			.and()
+ 				.formLogin().permitAll()
+ 			.and()
+ 				//login?logout
+ 				.logout().logoutSuccessUrl("/login").permitAll();
+ 	}
+	
+	@Autowired
+ 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+ 		auth
+ 			.inMemoryAuthentication()
+ 				.withUser("user").password("{noop}password").roles("USER")
+ 			.and()
+ 				.withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+ 	}
+	
+	
+	
+}
