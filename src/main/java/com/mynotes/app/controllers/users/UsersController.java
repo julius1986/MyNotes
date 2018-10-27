f10db95b8e213592874ef94b713b9865c2e34578
Yuliy <uliy86@gmail.com>
2018-10-27 15:07:15 +0200

    Init my project

---
+++ src/main/java/com/mynotes/app/controllers/users/UsersController.java
@@ -0,0 +1,26 @@
+package com.mynotes.app.controllers.users;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UsersController {
+
+	@GetMapping("test")
+	public String test() {
+		return "Test Users Controller Done";
+	}
+	
+	@GetMapping("public/test")
+	public String testPublic() {
+		return "Test Users Controller Done. This is public.";
+	}
+	
+	@GetMapping("private/test")
+	public String testPrivate() {
+		return "Test Users Controller Done. This is private.";
+	}
+	
+}
